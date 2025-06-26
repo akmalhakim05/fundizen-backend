3a3b2eadf3a018838fa4e3a8b78b87fe7ea5e536
AKMAL HAKIM BIN JAMALUDDIN <141492726+akmalhakim05@users.noreply.github.com>
2025-06-26 14:36:30 +0800

    Initial commit - Fundizen backend

---
+++ src/main/java/com/fundizen/fundizen_backend/FundizenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fundizen.fundizen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundizenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundizenBackendApplication.class, args);
+	}
+
+}
